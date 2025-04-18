a158ac298518ba342e7106513689e3b9d931b455
Aviat-at <akashtharindu7788@gmail.com>
2025-04-18 19:48:07 -0400

    update acction to push the code_update localhost host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'workwise_db'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'asd123'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
