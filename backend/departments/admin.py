32477a24cdee9ca7924529dc13dc199cdd00d0cd
Aviat-at <akashtharindu7788@gmail.com>
2025-04-18 16:29:57 -0400

    remove backend

--- backend/departments/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Department
-
-@admin.register(Department)
-class DepartmentAdmin(admin.ModelAdmin):
-    """Department Admin"""
-    list_display = ('name', 'created_at', 'updated_at')
-    search_fields = ('name',)
--- backend/departments/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DepartmentsConfig(AppConfig):
-    """DepartmentsConfig"""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'departments'
